ed9e6638e11ce1933c66d8c7616a0365e8ed5af5
alptuzun <alptuzun@sabanciuniv.edu>
2022-12-17 17:53:24 +0300

    Delete html düzeltildi

---
+++ delete.php
@@ -0,0 +1,13 @@
+<?php
+
+include "config.php";
+
+if (!empty($_POST['player_id'])) 
+{
+    $player_id = $_POST['player_id'];
+    $sql_statement = "DELETE FROM players WHERE player_id = $player_id";
+    $result = mysqli_query($db, $sql_statement);
+    echo "Your result is " . $result;
+}
+
+?>
